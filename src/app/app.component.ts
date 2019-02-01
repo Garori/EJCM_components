bb76438181a16a5ce828cadc8ea9f0a56afc68eb
Gabriel Conde <gabriel.wave.conde@gmail.com>
2019-01-31 23:56:51 -0200

    commit do trabalho de components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hero-project';
+  title = 'NHK';
+  youkoso = "ＮＨＫにようこそ！"
+
+  ngOnInit(){
+
+    console.log("もしもし、うぇえあぼおです")
+  }
 }
